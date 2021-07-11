a1cf1471fbdb224f312c13296d8e6d44b64b2580
KelpWrap <oscar.niles@gmx.de>
2021-07-11 13:53:43 +0200

    Set up basic structure for app. Started work on datapoint Component

---
+++ my-app/src/App.js
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import './App.css';
+import '.'
+import React, {Component} from 'react';
+import CanvasJSR from './lib/canvasjs-3.3.2/canvasjs.react'
+var CanvasJS = CanvasJSR.CanvasJS;
+var CanvasJSChart = CanvasJSR.CanvasJSChart;
+
+class App extends Component {
+	render() {
+		const options = {
+			animationEnabled: true,
+			title:{
+				text: "Monthly Sales - 2017"
+			},
+			axisX: {
+				valueFormatString: "MMM"
+			},
+			axisY: {
+				title: "Sales (in USD)",
+				prefix: "$"
+			},
+			data: [{
+				yValueFormatString: "$#,###",
+				xValueFormatString: "MMMM",
+				type: "spline",
+				dataPoints: [
+					{ x: new Date(2017, 0), y: 25060 },
+					{ x: new Date(2017, 1), y: 27980 },
+					{ x: new Date(2017, 2), y: 42800 },
+					{ x: new Date(2017, 3), y: 32400 },
+					{ x: new Date(2017, 4), y: 35260 },
+					{ x: new Date(2017, 5), y: 33900 },
+					{ x: new Date(2017, 6), y: 40000 },
+					{ x: new Date(2017, 7), y: 52500 },
+					{ x: new Date(2017, 8), y: 32300 },
+					{ x: new Date(2017, 9), y: 42000 },
+					{ x: new Date(2017, 10), y: 37160 },
+					{ x: new Date(2017, 11), y: 38400 }
+				]
+			}]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+}
+export default App;      
+ 
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          BipBipBoopBop.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
---
+++ my-app/src/component/DataPoint.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+export default class DataPoint extends React.Component{
+    static propTypes = {
+        date: PropTypes.string,
+        weight: PropTypes.number,
+        clickHandler: PropTypes.func,
+    };
+
+    handleClick = () => {
+        this.props.clickHandler(this.props.date, this.props.weight);
+    };
+
+    render() {
+        const className = [
+            "datapoint",
+            this.props.weight,
+        ];
+        return (
+            <div className={className.join(" ").trim()}>
+            <button onClick= {this.handleClick}>{this.props.date}{this.props.weight}</button>
+            </div>
+        );
+    }
+
+}
