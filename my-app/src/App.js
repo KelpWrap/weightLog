6197a94852f13187aeebe7036e9a0d78db7e41ca
KelpWrap <oscar.niles@gmx.de>
2021-07-16 22:27:10 +0200

    Implemented Modal for addButton

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-import '.'
-import DateFnsUtils from '@date-io/date-fns'
+import DateFnsUtils from '@date-io/date-fns';
 import React, {Component, useState} from 'react';
 import {DatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
-import graph from './component/Graph';
+import Graph from './component/Graph';
+import AddButton from './component/AddDatapointButton';
+import PopUp from './component/addDataModal';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+const Map = require('immutable');
 
+function getModalStyle(){
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        seen: false,
+        datapoints: Map,
+    }
+  };
 
-function App() {
-const [selectedDate, handleDateChange] = useState(new Date());
-  return (
+  togglePop = () => {
+   this.setState({
+    seen: !this.state.seen
+   });
+  };
 
-    <div className="App">
+  handleClick(){
+
+  };
+  
+//const [selectedDate, handleDateChange] = useState(new Date());
+//{<MuiPickersUtilsProvider utils = {DateFnsUtils}>   
+//  <DatePicker value={selectedDate} onChange={handleDateChange} />
+//</MuiPickersUtilsProvider>
+  render (){
+    return (<div className="App">
+      <AddButton/>
+      <Graph data={[
+            {date: '09-07-2021', weight: 73.7},
+            {date: '10-07-2021', weight: 70.7},
+            {date: '11-07-2021', weight: 68.6},
+            {date: '12-07-2021', weight: 68.3},
+            {date: '13-07-2021', weight: 68.3},
+            {date: '14-07-2021', weight: 67.7},
+            ]}/>
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        render(<graph/> )
-		<MuiPickersUtilsProvider utils = {DateFnsUtils}>      
-			<DatePicker value={selectedDate} onChange={handleDateChange} />
-		</MuiPickersUtilsProvider>
+        
 
+        <p>
           BipBipBoopBop.
         </p>
         <a
@@ -33,8 +71,10 @@ const [selectedDate, handleDateChange] = useState(new Date());
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
+      
+    </div>)
+  };
+};
+      
 
 export default App;
---
+++ my-app/src/component/AddDatapointButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {IconButton} from '@material-ui/core/';
+import Add from '@material-ui/icons/Add'
+
+function App(){
+    return <IconButton 
+    aria-label="addButton" >
+        <Add/>
+    </IconButton>
+}
+
+export default App;
